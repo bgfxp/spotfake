69237d03df87ce4d53019f1a14668497510d3d84
LUCAS BORGES COSTA DA SILVA <lucas_bc_silva@estudante.sesisenai.org.br>
2024-10-17 12:07:46 -0300

    spotifake

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import Express from 'express'
+
+const app = Express()
+
+app.get('/pegar', function(req, res){
+    res.send('enviar esta mensagem')
+})
+app.get('/pegaroutracoisa', function(req, res){
+    res.send('esta e outra mensagem')
+})
+
+
+app.listen(8000)                                                                              
